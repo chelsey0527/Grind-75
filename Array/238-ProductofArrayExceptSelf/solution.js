517aa995fea357ac1dcf28297be163991c1d6f65
Chelsey Lee <chelsey0527@gmail.com>
2023-06-09 13:39:13 +0800

    finish 39

--- Array/238-ProductofArrayExceptSelf/solution.js
+++ Array/238-ProductofArrayExceptSelf/solution.js
@@ -1,36 +1,30 @@
-# Contains Duplicate
-
-Problem can be found in [here](https://leetcode.com/problems/product-of-array-except-self/)!
-
-### [solution 1](/Array/238-ProductofArrayExceptSelf/solution.js): 
-
-```javascript
-var productExceptSelf = function(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
     const n = nums.length;
     const prefixProduct = new Array(n);
     const suffixProduct = new Array(n);
     const result = new Array(n);
-    
+
     // Calculate prefix products
     prefixProduct[0] = 1;
     for (let i = 1; i < n; i++) {
         prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
     }
-    
+
     // Calculate suffix products
     suffixProduct[n - 1] = 1;
     for (let i = n - 2; i >= 0; i--) {
         suffixProduct[i] = suffixProduct[i + 1] * nums[i + 1];
     }
-    
+
     // Calculate the product except self
     for (let i = 0; i < n; i++) {
         result[i] = prefixProduct[i] * suffixProduct[i];
     }
-    
+
     console.log(result);
     return result;
 };
-```
-
-Time Complexity: ![O(n^2)](<https://latex.codecogs.com/svg.image?\inline&space;O(n^2)>), Space Complexity: ![O(n)](<https://latex.codecogs.com/svg.image?\inline&space;O(n)>)
--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 -   [x] [Insert Interval](https://leetcode.com/problems/insert-interval): [Solution](/Array//57-InsertInterval/README.md)
 -   [x] [3Sum](https://leetcode.com/problems/3sum/): [Solution](/Array/15-3Sum/README.md)
 -   [x] [Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self): [Solution](/Array/238-ProductofArrayExceptSelf/README.md)
--   [ ] [Combination Sum](https://leetcode.com/problems/combination-sum): [Solution]
+-   [x] [Combination Sum](https://leetcode.com/problems/combination-sum): [Solution](/Array/39-CombinationSum/README.md)
 -   [ ] [Merge Intervals](https://leetcode.com/problems/merge-intervals): [Solution]
 -   [ ] [Sort Colors](https://leetcode.com/problems/sort-colors): [Solution]
 -   [ ] [Container With Most Water](https://leetcode.com/problems/container-with-most-water): [Solution]
